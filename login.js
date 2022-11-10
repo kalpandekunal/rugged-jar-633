e4ce371783d3d1585d58e17c1c25cb9257c3ddea
kalpandekunal <kalpandekunal007@gmail.com>
2022-11-10 17:56:06 +0530

    Day-3 completed

---
+++ login.js
@@ -0,0 +1,23 @@
+
+function checkData(){
+
+    var enterEmail = document.getElementById("email").value;
+    var enterPwd = document.getElementById("pwd").value;
+
+    var getEmail = localStorage.getItem("userEmail");
+    var getPwd = localStorage.getItem("userPwd");
+
+    if(enterEmail == getEmail){
+        if(enterPwd == getPwd){
+            
+            alert("Login Successful");
+            
+        }
+        else{
+            alert("wrong password");
+        }
+    }
+    else{
+        alert("Invalid details");
+    }
+}
---
+++ register.js
@@ -0,0 +1,10 @@
+
+
+function addData(){
+    var email = document.getElementById("email").value;
+    var pass =  document.getElementById("pwd").value;
+
+    localStorage.setItem("userEmail",email);
+    localStorage.setItem("userPwd",pass);
+
+}
